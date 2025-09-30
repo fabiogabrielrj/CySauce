77d669ba4b73b25b1771f469c2e834c656cd9635
Fabio <binfbgrj@gmail.com>
2025-09-30 10:08:13 -0300

    Atualizando confg

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ap6rr2',
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
     setupNodeEvents(on, config) {
